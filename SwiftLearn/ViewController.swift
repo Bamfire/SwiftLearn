fa2de1cd0fb8b11961bc6840d168296adb60aaef
bamfire <rabalos@gmail.com>
2017-05-29 22:54:39 -0700

    My first commit

---
+++ SwiftLearn/ViewController.swift
@@ -0,0 +1,44 @@
+//
+//  ViewController.swift
+//  SwiftLearn
+//
+//  Created by rommel on 5/23/17.
+//  Copyright © 2017 Rabalos. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var coolLabel: UILabel!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+    }
+     
+    var tapCount = 0
+    
+    @IBAction func buttonTap(_ sender: Any) {
+        tapCount = tapCount + 1
+        if tapCount >= 10 {
+            coolLabel.text = "You tapped the button 10 times!"
+    }
+
+    
+    
+   
+    }
+    
+    
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
